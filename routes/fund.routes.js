fc5a7e73c8286135f788e27e0a050751f6e5cd5a
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-11-08 01:07:34 +0530

    get organization funds error fixed

--- routes/fund.routes.js
+++ routes/fund.routes.js
@@ -19,7 +19,7 @@ router.put("/update/:id", updateFund);
 router.delete("/delete/:id", deleteFund);
 router.put("/remove/:id", removeFund);
 router.get("/status/:status", getFundByStatus);
-router.get("/:oranizationID/", getOrganizationFunds);
+router.get("/organizationfund/:id/", getOrganizationFunds);
 router.get("/:organizationID/status/:status", getFundByOrganizationAndStatus);
 
 module.exports = router;
